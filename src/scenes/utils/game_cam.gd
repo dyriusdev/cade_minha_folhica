b41a49aa0488098fb23a80b5e842a7c20f8ce4e9
Richardson <78909567+dyriusdev@users.noreply.github.com>
2024-12-11 13:13:54 -0300

    Implementação da camera

--- src/scenes/utils/game_cam.gd
+++
@@ -1,3 +0,0 @@
-extends Camera2D
-
-@export_node_path("Entity") var target_path : NodePath = ""
--- src/singletons/Events.gd
+++ src/singletons/Events.gd
@@ -4,5 +4,8 @@ para lidar com interação entre objetos
 """
 extends Node
 
+@warning_ignore("unused_signal")
 signal start_cutscene
+
+@warning_ignore("unused_signal")
 signal finished_cutscene
