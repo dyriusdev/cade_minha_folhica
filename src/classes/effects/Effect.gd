6f1d6d61f5c97492ec93250bccf11101a228263e
Richardson <78909567+dyriusdev@users.noreply.github.com>
2024-12-16 13:23:36 +0100

    Revert "Correção de pequenos erros"
    This reverts commit bdc2fa2417738649b8e4f4ce3373cdc72fa289d4.

---
+++ src/classes/effects/Effect.gd
@@ -0,0 +1,1 @@
+class_name Effect extends Node2D
--- src/singletons/Globals.gd
+++ src/singletons/Globals.gd
@@ -5,7 +5,3 @@ extends Node
 
 var loading_scene : PackedScene = preload("res://src/scenes/gui/loading.tscn")
 var next_scene = ""
-
-func _ready() -> void:
-	Logger.event("Starting game")
-	pass
